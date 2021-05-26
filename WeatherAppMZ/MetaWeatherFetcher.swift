f89e511dbda1a94ec508f4b319567c3458284850
ikantek <ikantek@github.com>
2021-05-26 03:47:54 -0700

    woeId added to localization

---
+++ WeatherAppMZ/MetaWeatherFetcher.swift
@@ -0,0 +1,21 @@
+//
+//  MetaWeatherFetcher.swift
+//  WeatherAppMZ
+//
+//  Created by Użytkownik Gość on 25/05/2021.
+//
+
+import Foundation
+import Combine
+
+class MetaWeatherFetcher{
+    
+    func forecast(forId woeId: String) -> AnyPublisher<MetaWeatherResponse, Error>{
+        let url = URL(string: "https://www.metaweather.com/api/location/\(woeId)/")!
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map{$0.data}
+            .decode(type: MetaWeatherResponse.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+    
+}
